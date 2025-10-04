40562fd5388bc5058e0c6777c9cd5696513598b1
DINESH S <dineshsomu1234@gmail.com>
2025-10-04 19:18:17 +0530

    Time: 6 ms (6.08%) | Memory: 58 MB (56.57%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.java
+++ 11-container-with-most-water/container-with-most-water.java
@@ -1,24 +1,47 @@
+import java.util.*;
 class Solution {
     public int maxArea(int[] height) {
-        int left=0;
-        int right=height.length-1;
-        int maxmium=0;
-        int minimum=0;
-        while(left<right){
+
+
+         int left=0;
+         int right=height.length-1;
+         int maximumarea=0;
+        int minimumheight=0;
+      
+
+         while(left<right){
             int width=right-left;
-            minimum=Math.min(height[left],height[right]);
-            int product=width*minimum;
-            maxmium=Math.max(maxmium,product);
-            if(height[left]<height[right]){
-                left++;
-            }
-            else{
-                right--;
-            }
+            minimumheight=Math.min(height[left],height[right]);
+         int  product=width*minimumheight;
+           maximumarea=Math.max(maximumarea,product);
+           if(height[left]<height[right]){
+            left++;
+           }else{
+           right--;}
 
 
-        }
-        return maxmium;
+         }
+         return maximumarea;
+
+        // int left=0;
+        // int right=height.length-1;
+        // int maxmium=0;
+        // int minimum=0;
+        // while(left<right){
+        //     int width=right-left;
+        //     minimum=Math.min(height[left],height[right]);
+        //     int product=width*minimum;
+        //     maxmium=Math.max(maxmium,product);
+        //     if(height[left]<height[right]){
+        //         left++;
+        //     }
+        //     else{
+        //         right--;
+        //     }
+
+
+        // }
+        // return maxmium;
         
     }
 }
