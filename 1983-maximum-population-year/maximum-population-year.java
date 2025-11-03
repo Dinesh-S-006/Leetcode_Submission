1fec8cc1883c0cd985e528ac6c00ad8551836170
DINESH S <dineshsomu1234@gmail.com>
2025-11-03 10:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (5.60%) - LeetSync

--- 1983-maximum-population-year/maximum-population-year.java
+++ 1983-maximum-population-year/maximum-population-year.java
@@ -1,26 +1,44 @@
 class Solution {
     public int maximumPopulation(int[][] logs) {
-        Map<Integer,Integer> map=new HashMap<>();
+        // Map<Integer,Integer> map=new HashMap<>();
 
-        for(int n[]:logs){
-            int birth=n[0];
-            int death=n[1]-1;
-            for(int i=birth;i<=death;i++){
-                map.put(i,map.getOrDefault(i,0)+1);
-            }
-        }
-        int maxyear=Integer.MAX_VALUE;
-        int maxpopulation=0;
-        for(int year:map.keySet()){
-            int population=map.get(year);
-            if(population>maxpopulation ||population==maxpopulation && year<maxyear){
-                maxyear=year;
-                maxpopulation=population;}}
+        // for(int n[]:logs){
+        //     int birth=n[0];
+        //     int death=n[1]-1;
+        //     for(int i=birth;i<=death;i++){
+        //         map.put(i,map.getOrDefault(i,0)+1);
+        //     }
+        // }
+        // int maxyear=Integer.MAX_VALUE;
+        // int maxpopulation=0;
+        // for(int year:map.keySet()){
+        //     int population=map.get(year);
+        //     if(population>maxpopulation ||population==maxpopulation && year<maxyear){
+        //         maxyear=year;
+        //         maxpopulation=population;}}
            
-           return maxyear;}
+        //    return maxyear;}
            
-            }
+        //     }
             
+            int arr[]=new int[2051];
+            for(int n[]:logs){
+                int birth=arr[n[0]]++;
+                int death=arr[n[1]]--;
+
+            }
+            int year=1950;
+            int pop=0;
+           int maxpop=0;
+            for(int i=1950;i<=2050;i++){
+                pop+=arr[i];
+                if(pop>maxpop){
+                    maxpop=pop;
+                    year=i;
+                }
+            }
+            return year;}}
+
             
             
             
