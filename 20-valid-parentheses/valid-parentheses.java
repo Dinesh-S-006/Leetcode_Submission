cc9a6d235c2aa0aa6d05f40187ef29500d1a6e08
DINESH S <dineshsomu1234@gmail.com>
2025-12-13 05:42:38 +0530

    Time: 3 ms (78.94%) | Memory: 43.1 MB (48.66%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>stack=new Stack();
+
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='['||ch=='{'||ch=='('){
+                stack.push(ch);
+            }
+            else{
+               if( stack.isEmpty()) return false;
+                char having=stack.pop();
+                if( ch==')' && having!='(' ||
+                ch=='}' && having!='{'||
+                 ch==']'&& having!='[' ){
+                    return false;
+                }
+            }}
+            return stack.isEmpty();
+
+        }
+        
+    }
