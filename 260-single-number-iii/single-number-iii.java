ded41bc261da76c4569d8721f553f3daef9b1925
DINESH S <dineshsomu1234@gmail.com>
2025-12-19 18:32:06 +0530

    Time: 1 ms (99.81%) | Memory: 48 MB (24.25%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -2,26 +2,38 @@ import java.util.*;
 
 class Solution {
     public int[] singleNumber(int[] nums) {
-        List<Integer> list = new ArrayList<>();
+        int xor=0;
+        for(int v:nums){ xor^=v;}
+        int rightmostbit=xor & -xor;
+        int a=0;
+        int b=0;
+        for(int n:nums){
+           if((n &rightmostbit)!=0){
+             a^=n;
+           }
+             else{
+                b^=n;
+             }}
+           
+           return new int[]{a,b};}}
+        // List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            int c = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (nums[i] == nums[j]) {
-                    c++;
-                }
-            }
-            if (c == 1) {
-                list.add(nums[i]);
-            }
-        }
+        // for (int i = 0; i < nums.length; i++) {
+        //     int c = 0;
+        //     for (int j = 0; j < nums.length; j++) {
+        //         if (nums[i] == nums[j]) {
+        //             c++;
+        //         }
+        //     }
+        //     if (c == 1) {
+        //         list.add(nums[i]);
+        //     }
+        // }
 
-        // Convert List<Integer> to int[]
-        int[] result = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            result[i] = list.get(i);
-        }
+        // int[] result = new int[list.size()];
+        // for (int i = 0; i < list.size(); i++) {
+        //     result[i] = list.get(i);
+        // }
 
-        return result;
-    }
-}
+        // return result;
+
