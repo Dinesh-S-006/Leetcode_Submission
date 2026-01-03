003a4a254cc4f5981088739138d93e387bbbb918
DINESH S <dineshsomu1234@gmail.com>
2026-01-03 18:03:17 +0530

    Time: 7 ms (58.65%) | Memory: 47.2 MB (21.94%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -3,14 +3,15 @@ class Solution {
         int n=s.length();
         if(n==1) return 0;
         int freq[]=new int[26];
+        Queue<Integer>q=new LinkedList<>();
         for(char ch:s.toCharArray()){
             freq[ch-'a']++;
         }
         for(int i=0;i<n;i++){
             if(freq[s.charAt(i)-'a']==1){
-                return i;
+                q.offer(i);
             }
         }
-        return -1;
+        return (q.isEmpty())?-1:q.peek();
     }
 }
