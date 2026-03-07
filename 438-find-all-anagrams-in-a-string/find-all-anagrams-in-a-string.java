7ae6d431681c21febb11d62b384c427590309cd8
DINESH S <dineshsomu1234@gmail.com>
2026-03-07 22:49:00 +0530

    Time: 11 ms (69.21%) | Memory: 46.7 MB (81.43%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,31 +1,94 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-  
-      List<Integer> result=new ArrayList<>();
-            if(s.length()<p.length()){return result;}
-      int s_arr[]=new int[26];
-      int p_arr[]=new int[26];
-      for(int ch:p.toCharArray()){
-        p_arr[ch-'a']++;
+        List<Integer>result=new ArrayList<>();
+        if(p.length()>s.length()){
+            return result;
+        }
+        int Stringarr[]=new int[26];
+        int Patternarr[]=new int[26];
+        int window=p.length();
+        for(int i=0;i<window;i++){
+            Patternarr[p.charAt(i)-'a']++;
+        }
+        for(int i=0;i<window;i++){
+       
+            Stringarr[s.charAt(i)-'a']++;}
+ 
+      if(Arrays.equals(Patternarr,Stringarr)){
+        result.add(0);
 
       }
-      int window=p.length();
-      for(int i=0;i<window;i++){
-        s_arr[s.charAt(i)-'a']++;
-      }
-      if(Arrays.equals(p_arr,s_arr)){
-        result.add(0);
-      }
       for(int i=window;i<s.length();i++){
-        s_arr[s.charAt(i)-'a']++;
-        s_arr[s.charAt(i-window)-'a']--;
-        if(Arrays.equals(p_arr,s_arr)){
+         Stringarr[s.charAt(i)-'a']++;
+         Stringarr[s.charAt(i-window)-'a']--;
+
+      
+        if(Arrays.equals(Patternarr,Stringarr)){
         result.add(i-window+1);
+        }
       }
-      }
-      return result;
+
+      return result;}}
+
+          
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //   List<Integer> result=new ArrayList<>();
+
+    //         if(s.length()<p.length()){return result;}
+    //   int s_arr[]=new int[26];
+    //   int p_arr[]=new int[26];
+    //   for(int ch:p.toCharArray()){
+    //     p_arr[ch-'a']++;
+
+    //   }
+    //   int window=p.length();
+    //   for(int i=0;i<window;i++){
+    //     s_arr[s.charAt(i)-'a']++;
+    //   }
+    //   if(Arrays.equals(p_arr,s_arr)){
+    //     result.add(0);
+    //   }
+    //   for(int i=window;i<s.length();i++){
+    //     s_arr[s.charAt(i)-'a']++;
+    //     s_arr[s.charAt(i-window)-'a']--;
+    //     if(Arrays.equals(p_arr,s_arr)){
+    //     result.add(i-window+1);
+    //   }
+    //   }
+    //   return result;
 
         
 
-      }
-    }
+    //   }
+    // }
