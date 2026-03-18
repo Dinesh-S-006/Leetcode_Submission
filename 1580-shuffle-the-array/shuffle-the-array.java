27c8c191dc7efc1f2fbb01ccb2870a62d983bde5
DINESH S <dineshsomu1234@gmail.com>
2026-03-18 20:01:59 +0530

    Time: 3 ms (1.90%) | Memory: 46.6 MB (58.81%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.java
+++ 1580-shuffle-the-array/shuffle-the-array.java
@@ -1,11 +1,79 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int result[]=new int[2*n];
-        int k=0;
-        for(int i=0;i<n;i++){
-            result[k++]=nums[i];
-            result[k++]=nums[i+n];
-        }
-        return result;
+    List<Integer>l1=new ArrayList<>();
+    List<Integer>l2=new ArrayList<>();
+    for(int i=0;i<n;i++) l1.add(nums[i]);
+    for(int i=n;i<(2*n);i++) l2.add(nums[i]);
+    int newarr[]=new int[2*n];
+    int p=0;
+    for(int i=0;i<l1.size();i++){
+
+        newarr[p++]=l1.get(i);
+        newarr[p++]=l2.get(i);
     }
-}
+     return newarr;}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // int arr[]=new int[2*n];
+        // int left=0;
+        // int right=left+n;
+        // int p=0;
+        // int c=0;
+    
+        // while(c<n){
+        //     arr[p]=nums[left];
+        //     arr[p]=nums[right];
+        //        c++;
+        //     if(c<n){
+        //     left++;
+        //     right++;}
+         
+        // }
+        // return arr;}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //     int result[]=new int[2*n];
+
+    //     int k=0;
+    //     for(int i=0;i<n;i++){
+    //         result[k++]=nums[i];
+    //         result[k++]=nums[i+n];
+    //     }
+    //     return result;
+    // }
