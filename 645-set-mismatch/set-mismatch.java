c5220900c1f9141a4dc5d7daea58e8341f885670
DINESH S <dineshsomu1234@gmail.com>
2026-03-19 10:44:19 +0530

    Time: 16 ms (18.80%) | Memory: 48.5 MB (6.43%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,25 +1,60 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        Arrays.sort(nums);
+        int missingNumber=-1;
+        int duplicateNumber=-1;
         int n=nums.length;
-        int dup=-1;
-        int miss=1;
 
-     
+        Arrays.sort(nums);
         for(int i=1;i<nums.length;i++){
             if(nums[i-1]==nums[i]){
-               dup=nums[i];}
-              else if(nums[i]>nums[i-1]+1){
-                miss=nums[i-1]+1;
-                
-              }
+                duplicateNumber=nums[i];
+            }
+            if((nums[i])>nums[i-1]+1){
+                missingNumber=nums[i-1]+1;
+            }
+        }
 
-            }
-            if(nums[n-1]!=n){
-                miss=n;
-            }
+        if(nums[n-1]!=n){
+             missingNumber=n;
+
+        }
+        if(nums[0]!=1){
+            missingNumber=1;
+        }
+
+        return new int[]{duplicateNumber,missingNumber};}}
+
+     
+
+
+
+
+
+
+
+
+
+
+//         Arrays.sort(nums);
+//         int n=nums.length;
+//         int dup=-1;
+//         int miss=1;
+
+     
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i-1]==nums[i]){
+//                dup=nums[i];}
+//               else if(nums[i]>nums[i-1]+1){
+//                 miss=nums[i-1]+1;
+                
+//               }
+
+//             }
+//             if(nums[n-1]!=n){
+//                 miss=n;
+//             }
         
-        return new int[]{dup,miss};
+//         return new int[]{dup,miss};
         
-    }
-}
+//     }
+// }
