f985967078f9231ecb8594864786d9d5e5f4e463
DINESH S <dineshsomu1234@gmail.com>
2026-03-19 11:21:40 +0530

    Time: 7 ms (40.67%) | Memory: 45.5 MB (40.95%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,24 +1,71 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer>stack=new Stack<>();
-      
-        for(String s:tokens){
-            if(s.equals("+") || s.equals("-")|| s.equals("*") || s.equals("/")){
+        Stack<Integer> stack=new Stack<>();
+        for(String ch:tokens){
+            if(ch.equals("+") || ch.equals("-") || ch.equals("*") || ch.equals("/")){
+                int res=0;
                 int b=stack.pop();
                 int a=stack.pop();
-                  int res=0;
-                switch(s){
-                case"+":res=a+b;break;
-                case"-":res=a-b;break;
-                case"*":res=a*b;break;
-                case"/":res=a/b;break;
-            }
-            stack.push(res);}
-            else{
-                stack.push(Integer.parseInt(s));
-            }
+                switch(ch){
+                    case "+":res=a+b;
+                            break;
+                    case "-":res=a-b;
+                             break;
+                    case "*":res=a*b;
+                             break;
+                    case "/":res=a/b;
+                             break;
+                }
+                stack.push(res);
 
+            }
+            else{stack.push(Integer.parseInt(ch));}
         }
-        return stack.pop();
-    }
-}
+        return stack.pop();}}
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         Stack<Integer>stack=new Stack<>();
+      
+//         for(String s:tokens){
+//             if(s.equals("+") || s.equals("-")|| s.equals("*") || s.equals("/")){
+//                 int b=stack.pop();
+//                 int a=stack.pop();
+//                   int res=0;
+//                 switch(s){
+//                 case"+":res=a+b;break;
+//                 case"-":res=a-b;break;
+//                 case"*":res=a*b;break;
+//                 case"/":res=a/b;break;
+//             }
+//             stack.push(res);}
+//             else{
+//                 stack.push(Integer.parseInt(s));
+//             }
+
+//         }
+//         return stack.pop();
+//     }
+// }
