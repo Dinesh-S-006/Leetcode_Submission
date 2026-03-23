c53f953d3c93242d0dfcd4ef1bc5d37cd501a3d9
DINESH S <dineshsomu1234@gmail.com>
2026-03-23 20:37:09 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (9.49%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -10,15 +10,15 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode curr=head;
-        ListNode prev=null;
-        while(curr!=null){
-            ListNode next=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=next;
-        }
-      return prev;
+     ListNode curr=head;
+     ListNode prev=null;
+     ListNode next;
+     while(curr!=null){
+        next=curr.next;
+        curr.next=prev;
+        prev=curr;
+        curr=next;
+     }
+        return prev;}
         
     }
-}
