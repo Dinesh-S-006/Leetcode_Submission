30e507b5ae9c271349b730dfce80fc09796260d9
DINESH S <dineshsomu1234@gmail.com>
2026-04-05 22:35:47 +0530

    Time: 1 ms (98.77%) | Memory: 44.3 MB (86.89%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPossibleToSplit(int[] nums) {
+        int arr[]=new int[101];
+        for(int n:nums){
+            arr[n]++;
+            if(arr[n]>2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
