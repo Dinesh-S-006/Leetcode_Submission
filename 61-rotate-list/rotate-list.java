d965570c44b031b506ed1aa12d21134caa4730a8
DINESH S <dineshsomu1234@gmail.com>
2026-05-05 10:28:41 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (90.09%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -8,31 +8,92 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ /*
+length
+step
+nodes
+
+
+
+ */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
+        //
         if(head==null || head.next==null || k==0){
             return head;
         }
-        // if(head.next==null){
-        //     return head;
-        // }
-        int length=1;
-        ListNode temp=head;
-        while(temp.next!=null){
-            temp=temp.next;
-            length++;
-        }
- temp.next=head;
-        ListNode newTail=head;
-        int numberofStep=length-(k%length);
-
-
-        while(numberofStep>1){
-            newTail=newTail.next;
-            numberofStep--;
-        }
-        ListNode newHead=newTail.next;
-        newTail.next=null;
-         return newHead;
+        // length
+    int length=1;
+    ListNode temp=head;
+    while(temp.next!=null){
+        length++;
+        temp=temp.next;
     }
-}
+    temp.next=head;
+    ListNode newnode_1=head;
+
+    int number_of_Step=length-(k%length);
+    for(int i=1;i<number_of_Step;i++){
+          newnode_1=newnode_1.next;
+    }
+    ListNode newnode_2=newnode_1.next;
+    newnode_1.next=null;
+    return newnode_2;}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         if(head==null || head.next==null || k==0){
+//             return head;
+//         }
+//         // if(head.next==null){
+//         //     return head;
+//         // }
+//         int length=1;
+//         ListNode temp=head;
+//         while(temp.next!=null){
+//             temp=temp.next;
+//             length++;
+//         }
+//  temp.next=head;
+//         ListNode newTail=head;
+//         int numberofStep=length-(k%length);
+//         for(int i=0;i<numberofStep;i++){
+//             newTail=newTail.next;
+//         }
+
+//         // while(numberofStep>1){
+//         //     newTail=newTail.next;
+//         //     numberofStep--;
+//         // }
+//         ListNode newHead=newTail.next;
+//         newTail.next=null;
+//          return newHead;
+//     }
+// }
